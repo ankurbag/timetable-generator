6a22eb1831dfde46ceb1d2683edfb1136fea88d5
Ankur Bag <bagankur@gmail.com>
2017-12-04 14:40:04 -0500

    Adding initial Spring REST Project

---
+++ genetic-algo-api/src/main/java/edu/neu/ga/response/BotResponse.java
@@ -0,0 +1,14 @@
+package edu.neu.ga.response;
+
+public class BotResponse{
+	private String speech;
+
+	public String getSpeech() {
+		return speech;
+	}
+
+	public void setSpeech(String speech) {
+		this.speech = speech;
+	}
+	
+}
