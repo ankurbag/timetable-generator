3ec117381d5e771669e67fcf39436f0fd6403c9c
Ankur Bag <bagankur@gmail.com>
2017-12-05 02:09:43 -0500

    Added Timetable controller

---
+++ genetic-algo-api/src/main/java/edu/neu/ga/controller/TimetableController.java
@@ -0,0 +1,40 @@
+package edu.neu.ga.controller;
+
+import java.sql.Connection;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.neu.ga.service.TimetableService;
+import io.swagger.annotations.Api;
+import edu.neu.ga.beans.*;
+import edu.neu.ga.beans.Class;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@Api(value = "Timetable", description = "Timetable Controller")
+@RequestMapping("/classes")
+public class TimetableController {
+
+	private static final Logger logger = LoggerFactory.getLogger(TimetableController.class);
+
+	/**
+	 * Get the list of Classes
+	 */
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public List<Class> getClasses() {
+		logger.info("Invoke : TimetableController.");
+		return new TimetableService().execute();
+	}
+
+
+}
--- genetic-algo-api/src/main/java/edu/neu/ga/service/TimetableService.java
+++ genetic-algo-api/src/main/java/edu/neu/ga/service/TimetableService.java
@@ -1,9 +1,16 @@
 package edu.neu.ga.service;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.neu.ga.beans.Class;
 import edu.neu.ga.beans.GeneticAlgorithm;
 import edu.neu.ga.beans.Population;
 import edu.neu.ga.beans.Timetable;
+import edu.neu.ga.controller.TimetableController;
 
 /**
  * Don't be daunted by the number of classes in this chapter -- most of them are
@@ -23,8 +30,8 @@ import edu.neu.ga.beans.Timetable;
  * two classes simultaneously), and so is used by the GeneticAlgorithm's
  * calcFitness class as well.
  * 
- * Finally, we overload the Timetable class by entrusting it with the
- * "database information" generated here in initializeTimetable. Normally, that
+ * Finally, we overload the Timetable class by entrusting it with the "database
+ * information" generated here in initializeTimetable. Normally, that
  * information about what professors are employed and which classrooms the
  * university has would come from a database, but this isn't a book about
  * databases so we hardcode it.
@@ -34,77 +41,86 @@ import edu.neu.ga.beans.Timetable;
  */
 public class TimetableService {
 
-    public static void main(String[] args) {
-    	// Get a Timetable object with all the available information.
-        Timetable timetable = initializeTimetable();
-        
-        // Initialize GA
-        GeneticAlgorithm ga = new GeneticAlgorithm(100, 0.01, 0.9, 2, 5);
-        
-        // Initialize population
-        Population population = ga.initPopulation(timetable);
-        
-        // Evaluate population
-        ga.evalPopulation(population, timetable);
-        
-        // Keep track of current generation
-        int generation = 1;
-        
-        // Start evolution loop
-        while (ga.isTerminationConditionMet(generation, 1000) == false
-            && ga.isTerminationConditionMet(population) == false) {
-            // Print fitness
-            System.out.println("G" + generation + " Best fitness: " + population.getFittest(0).getFitness());
+	private Timetable timetable;
+	private GeneticAlgorithm ga;
+	private Population population;
+	private int generation;
 
-            // Apply crossover
-            population = ga.crossoverPopulation(population);
+	private static final Logger logger = LoggerFactory.getLogger(TimetableService.class);
 
-            // Apply mutation
-            population = ga.mutatePopulation(population, timetable);
+	
+	public TimetableService() {
+		timetable = initializeTimetable();
 
-            // Evaluate population
-            ga.evalPopulation(population, timetable);
+		// Initialize GA
+		ga = new GeneticAlgorithm(100, 0.01, 0.9, 2, 5);
 
-            // Increment the current generation
-            generation++;
-        }
+		// Initialize population
+		population = ga.initPopulation(timetable);
 
-        // Print fitness
-        timetable.createClasses(population.getFittest(0));
-        System.out.println();
-        System.out.println("Solution found in " + generation + " generations");
-        System.out.println("Final solution fitness: " + population.getFittest(0).getFitness());
-        System.out.println("Clashes: " + timetable.calcClashes());
+		// Evaluate population
+		ga.evalPopulation(population, timetable);
 
-        // Print classes
-        System.out.println();
-        Class classes[] = timetable.getClasses();
-        int classIndex = 1;
-        for (Class bestClass : classes) {
-            System.out.println("Class " + classIndex + ":");
-            System.out.println("Module: " + 
-                    timetable.getModule(bestClass.getModuleId()).getModuleName());
-            System.out.println("Group: " + 
-                    timetable.getGroup(bestClass.getGroupId()).getGroupId());
-            System.out.println("Room: " + 
-                    timetable.getRoom(bestClass.getRoomId()).getRoomNumber());
-            System.out.println("Professor: " + 
-                    timetable.getProfessor(bestClass.getProfessorId()).getProfessorName());
-            System.out.println("Time: " + 
-                    timetable.getTimeslot(bestClass.getTimeslotId()).getTimeslot());
-            System.out.println("-----");
-            classIndex++;
-        }
-    }
+		// Keep track of current generation
+		generation = 1;
+	}
 
-    /**
-     * Creates a Timetable with all the necessary course information.
-     * 
-     * Normally you'd get this info from a database.
-     * 
-     * @return
-     */
-	private static Timetable initializeTimetable() {
+	/**
+	 * Returns Arrays of Class
+	 * 
+	 * @return
+	 */
+	public List<Class> execute() {
+
+		// Start evolution loop
+		while (ga.isTerminationConditionMet(generation, 1000) == false
+				&& ga.isTerminationConditionMet(population) == false) {
+			// Print fitness
+			logger.info("G" + generation + " Best fitness: " + population.getFittest(0).getFitness());
+
+			// Apply crossover
+			population = ga.crossoverPopulation(population);
+
+			// Apply mutation
+			population = ga.mutatePopulation(population, timetable);
+
+			// Evaluate population
+			ga.evalPopulation(population, timetable);
+
+			// Increment the current generation
+			generation++;
+		}
+
+		// Print fitness
+		timetable.createClasses(population.getFittest(0));
+		logger.info("Solution found in " + generation + " generations");
+		logger.info("Final solution fitness: " + population.getFittest(0).getFitness());
+		logger.info("Clashes: " + timetable.calcClashes());
+
+		// Print classes
+		Class classes[] = timetable.getClasses();
+		int classIndex = 1;
+		for (Class bestClass : classes) {
+			logger.info("Class " + classIndex + ":");
+			logger.info("Module: " + timetable.getModule(bestClass.getModuleId()).getModuleName());
+			logger.info("Group: " + timetable.getGroup(bestClass.getGroupId()).getGroupId());
+			logger.info("Room: " + timetable.getRoom(bestClass.getRoomId()).getRoomNumber());
+			logger.info("Professor: " + timetable.getProfessor(bestClass.getProfessorId()).getProfessorName());
+			logger.info("Time: " + timetable.getTimeslot(bestClass.getTimeslotId()).getTimeslot());
+			logger.info("-----");
+			classIndex++;
+		}
+		return Arrays.asList(classes);
+	}
+
+	/**
+	 * Creates a Timetable with all the necessary course information.
+	 * 
+	 * Normally you'd get this info from a database.
+	 * 
+	 * @return
+	 */
+	private Timetable initializeTimetable() {
 		// Create timetable
 		Timetable timetable = new Timetable();
 
